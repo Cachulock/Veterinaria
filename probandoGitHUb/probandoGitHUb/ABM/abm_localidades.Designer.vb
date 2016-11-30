dd357ff8954d23ac201e631fa94c0082e32fe81f
loledesma <lorenasoledadledesma@gmail.com>
2016-11-30 15:58:04 -0300

    cambioGreta1

--- probandoGitHUb/probandoGitHUb/ABM/abm_localidades.Designer.vb
+++ probandoGitHUb/probandoGitHUb/ABM/abm_localidades.Designer.vb
@@ -22,16 +22,39 @@ Partial Class abm_localidades
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
         Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(37, 22)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Puto"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(140, 50)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(120, 95)
+        Me.ListBox1.TabIndex = 1
         '
         'abm_localidades
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.ClientSize = New System.Drawing.Size(385, 308)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.Button1)
         Me.Name = "abm_localidades"
         Me.Text = "abm_localidades"
         Me.ResumeLayout(False)
 
     End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
 End Class
