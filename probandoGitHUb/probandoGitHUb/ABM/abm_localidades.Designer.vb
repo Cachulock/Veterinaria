2e6de32725952e7a5ecabad848a93710fe51180b
loledesma <lorenasoledadledesma@gmail.com>
2016-11-30 16:24:27 -0300

    cambio6

--- probandoGitHUb/probandoGitHUb/ABM/abm_localidades.Designer.vb
+++ probandoGitHUb/probandoGitHUb/ABM/abm_localidades.Designer.vb
@@ -40,14 +40,14 @@ Partial Class abm_localidades
         Me.ListBox1.FormattingEnabled = True
         Me.ListBox1.Location = New System.Drawing.Point(144, 61)
         Me.ListBox1.Name = "ListBox1"
-        Me.ListBox1.Size = New System.Drawing.Size(216, 212)
+        Me.ListBox1.Size = New System.Drawing.Size(361, 212)
         Me.ListBox1.TabIndex = 1
         '
         'abm_localidades
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(385, 308)
+        Me.ClientSize = New System.Drawing.Size(536, 308)
         Me.Controls.Add(Me.ListBox1)
         Me.Controls.Add(Me.Button1)
         Me.Name = "abm_localidades"
