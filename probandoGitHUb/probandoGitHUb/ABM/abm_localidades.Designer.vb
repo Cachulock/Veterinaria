eac18b0795c91046453cc2a34311de40bcca3a90
loledesma <lorenasoledadledesma@gmail.com>
2016-12-01 09:00:20 -0300

    cambioLore1

--- probandoGitHUb/probandoGitHUb/ABM/abm_localidades.Designer.vb
+++ probandoGitHUb/probandoGitHUb/ABM/abm_localidades.Designer.vb
@@ -28,7 +28,7 @@ Partial Class abm_localidades
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(37, 22)
+        Me.Button1.Location = New System.Drawing.Point(98, 23)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(75, 23)
         Me.Button1.TabIndex = 0
@@ -38,16 +38,16 @@ Partial Class abm_localidades
         'ListBox1
         '
         Me.ListBox1.FormattingEnabled = True
-        Me.ListBox1.Location = New System.Drawing.Point(26, 75)
+        Me.ListBox1.Location = New System.Drawing.Point(18, 75)
         Me.ListBox1.Name = "ListBox1"
-        Me.ListBox1.Size = New System.Drawing.Size(753, 329)
+        Me.ListBox1.Size = New System.Drawing.Size(244, 329)
         Me.ListBox1.TabIndex = 1
         '
         'abm_localidades
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(809, 433)
+        Me.ClientSize = New System.Drawing.Size(283, 433)
         Me.Controls.Add(Me.ListBox1)
         Me.Controls.Add(Me.Button1)
         Me.Name = "abm_localidades"
